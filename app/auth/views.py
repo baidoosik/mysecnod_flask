8af7633b2719976c26b99b06835e730fda9033aa
baidoosik <qoentlr37@naver.com>
2017-08-31 23:19:12 +0900

    회원가입 후 이메일 인증, 이메일 토큰 발송

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,9 +1,11 @@
+import threading
 from flask import render_template, redirect, request, url_for, flash
-from flask_login import login_user, logout_user, login_required
+from flask_login import login_user, logout_user, login_required, current_user
 from . import auth
 from .. import db
 from ..models import User
 from .forms import LoginForm,RegistrationForm
+from ..email import Mail
 
 @auth.route('/login',methods=['GET','POST'])
 def login():
@@ -32,9 +34,31 @@ def register():
     if form.validate_on_submit():
         user = User(user_name=form.username.data,
                     email=form.email.data,
-                    password=form.password.data)
+                    password=form.password.data
+                    )
         db.session.add(user)
-        flash('finish sign up process')
+        db.session.commit()
+
+        token= user.generate_confirmation_token()
+        token_url = 'http://127.0.0.1:5000/auth/'+str(token)
+        mail=Mail(token_url,user.email)
+        t=threading.Thread(target=mail.naver_send_email) # 다른 스레드이용.
+        t.start()
+        flash('finish sign up process check your mail')
+
         return redirect(url_for('auth.login'))
 
     return render_template('auth/register.html',form=form)
+
+@auth.route('/confirm/<token>')
+def confirm(token):
+    if current_user.confirmed:
+        return redirect(url_for('main.index'))
+
+    if current_user.generate_confirmation_token(token):
+        flash("you success to register!")
+
+    else:
+        flash("your link is invalid")
+
+    return redirect(url_for('main.index'))
--- app/models.py
+++ app/models.py
@@ -1,4 +1,6 @@
 from werkzeug.security import generate_password_hash, check_password_hash
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from flask import current_app
 from flask_login import UserMixin
 from . import db,login_manager
 
@@ -20,6 +22,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(64),unique=True,index=True)
     password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    confirmed = db.Column(db.Boolean, default=False)
+
 
 
     @property
@@ -35,6 +39,22 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<USER {}>'.format(self.user_name)
+
+    def generate_confirmation_token(self,expiration=3600):
+        s = Serializer(current_app.config['SECRET_KEY'],expiration)
+        return s.dumps({'confirm':self.id})
+
+    def confirm(self,token):
+        s= Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data =s.loads(token)
+        except:
+            return False
+
+        self.confirmed=True
+        db.session.add(self)
+
+        return True
 
 
 @login_manager.user_loader

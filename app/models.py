8cbfef609650eb79fb44c3817056d498975ecb67
baidoosik <qoentlr37@naver.com>
2017-09-18 09:27:22 +0900

    api앱 authentication 부분

--- app/models.py
+++ app/models.py
@@ -130,6 +130,19 @@ class User(UserMixin, db.Model):
         token = token.decode('utf-8')
         return token
 
+    def generate_auth_token(self, expiration):
+        s = Serializer(current_app.config['SECRET_KEY'],expires_in=expiration)
+        return s.dumps({'id': self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return  User.query.get(data['id'])
+
     def confirm(self,token):
         s= Serializer(current_app.config['SECRET_KEY'])
         try:
